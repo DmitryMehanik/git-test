de0331e3230d76133244721a167636cf238e219b
Dmitry <Dmitry.suprun.mehanik@gmail.com>
2016-07-02 16:02:39 +0300

    add

---
+++ forma.php
@@ -0,0 +1,17 @@
+<form>
+
+<input type="text" value="Введите текст" size="50px">
+    <input type="button" value="Кнопка">
+    
+    <?php
+    
+    echo "Форма заполнения";
+     echo "Форма заполнения";
+     echo "Форма заполнения";
+    
+    ?>
+    
+
+
+
+</form>
